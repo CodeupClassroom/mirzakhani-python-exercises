7235b71ad38776d2a1c74e69c6515297ee1a2cd7
Andrew King <andrewking2713@gmail.com>
2022-09-22 14:46:28 -0500

    added import exercises material

---
+++ import_tip.py
@@ -0,0 +1,3 @@
+import my_functions as mf
+tip = mf.calculate_tip(30)
+print(tip)
--- my_functions.py
+++ my_functions.py
@@ -1,2 +1,26 @@
 def iterate(n):
     return n + 1
+
+def is_vowel(somestring):
+    if type(somestring) == str:
+        if len(somestring) == 1 and somestring.isalpha():
+            return somestring.lower() in list('aeiou')
+        else:
+            return False
+    else:
+        return False
+    
+def calculate_tip(bill, tip_percent = 0.2):
+    return bill * tip_percent
+
+def get_letter_grade(somegrade):
+    if somegrade >= 90:
+        return 'A'
+    elif somegrade >= 80:
+        return 'B'
+    elif somegrade >= 70:
+        return 'C'
+    elif somegrade >= 60:
+        return 'D'
+    else:
+        return 'F'
